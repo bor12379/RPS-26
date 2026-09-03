8324038d540175351b31e0e6f37f8989a73eda21
bor12379 <bor.gouverneur1@gmail.com>
2026-09-03 09:45:30 +0000

    prva vaja

--- vaja/uvod.py
+++ vaja/uvod.py
@@ -1,1 +1,13 @@
-print("hello world")
+
+def hello():
+    o = input("kateri oddelek si")
+    if o.lower() == "1.ri":
+        print(f"hello {o} 3")
+    else:
+        print(f"hello {o} ")
+
+
+
+if __name__ =="__main__":
+    hello()
+
