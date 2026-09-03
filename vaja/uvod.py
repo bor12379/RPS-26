e6767e82fcbb06b6c641200f2c07b06385727af4
bor12379 <bor.gouverneur1@gmail.com>
2026-09-03 10:00:37 +0000

    2 vaja

--- vaja/uvod.py
+++ vaja/uvod.py
@@ -6,8 +6,14 @@ def hello():
     else:
         print(f"hello {o} ")
 
-
+def poštevanka():
+    x = int(input("izberi si število: "))
+    a = 1
+    while a <=10:
+        print("...")
+        a +=1
 
 if __name__ =="__main__":
-    hello()
+    # hello()
+    poštevanka()
 
